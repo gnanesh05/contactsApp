8d4df215bb041e648f4a361fc99a2cb193d4df71
gnanesmk <gnaneshwer.mk@sscinc.com>
2025-01-16 19:43:52 +0530

    netlify changes

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -3,13 +3,48 @@ import serverless from "serverless-http";
 import swaggerUI from "swagger-ui-express";
 import contactsRoute from "../../routes/contacts.js";
 import usersRoute from "../../routes/users.js";
-import swaggerSpec from "../../swagger.js";
 
 // Initialize express app
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+//Swagger UI
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "ContactsAPI",
+      version: "1.0.0",
+      description: "API documentation for ContactAPP",
+    },
+    servers: [
+      {
+        url: "https://contactsapi.netlify.app/.netlify/functions/app", // Replace with your Netlify deployment URL
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT", // Indicates JSON Web Tokens
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [], // Apply the scheme globally
+      },
+    ],
+  },
+  apis: ["../../routes/users.js", "../../routes/contacts.js"], // Include your routes here
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 // Define Routes
 app.use("/api/contacts", contactsRoute);
