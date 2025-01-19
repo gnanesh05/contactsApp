decb15e7fcc73071ca8b1e413a8a7f0abfa240b6
gnanesmk <gnaneshwer.mk@sscinc.com>
2025-01-19 22:11:04 +0530

    adding only api routes

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import serverless from "serverless-http";
-import swaggerUI from "swagger-ui-express";
+// import swaggerUI from "swagger-ui-express";
 import contactsRoute from "../../routes/contacts.js";
 import usersRoute from "../../routes/users.js";
 
@@ -11,45 +11,45 @@ const app = express();
 app.use(express.json());
 
 //Swagger UI
-import swaggerJSDoc from "swagger-jsdoc";
+// import swaggerJSDoc from "swagger-jsdoc";
 
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "ContactsAPI",
-      version: "1.0.0",
-      description: "API documentation for ContactAPP",
-    },
-    servers: [
-      {
-        url: "https://contactsapi.netlify.app/.netlify/functions/app", // Replace with your Netlify deployment URL
-      },
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "http",
-          scheme: "bearer",
-          bearerFormat: "JWT", // Indicates JSON Web Tokens
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [], // Apply the scheme globally
-      },
-    ],
-  },
-  apis: ["../../routes/users.js", "../../routes/contacts.js"], // Include your routes here
-};
+// const swaggerOptions = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "ContactsAPI",
+//       version: "1.0.0",
+//       description: "API documentation for ContactAPP",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:8888/", // Replace with your Netlify deployment URL
+//       },
+//     ],
+//     components: {
+//       securitySchemes: {
+//         bearerAuth: {
+//           type: "http",
+//           scheme: "bearer",
+//           bearerFormat: "JWT", // Indicates JSON Web Tokens
+//         },
+//       },
+//     },
+//     security: [
+//       {
+//         bearerAuth: [], // Apply the scheme globally
+//       },
+//     ],
+//   },
+//   apis: ["../../routes/users.js", "../../routes/contacts.js"], // Include your routes here
+// };
 
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
+// const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 // Define Routes
 app.use("/api/contacts", contactsRoute);
 app.use("/api/users", usersRoute);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+//app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 // Simple Hello World Route
 app.get("/hello", (req, res) => res.send("Hello World!"));
